bbf7d4140742cce3869990ef88a2aff82115b0cb
Ayeszxaa <130112710+Ayeszxaa@users.noreply.github.com>
2023-06-14 22:58:40 +0800

    ARRAY

--- iceScramble/Program.cs
+++ iceScramble/Program.cs
@@ -6,140 +6,68 @@ class IceScramble
     {
         Console.WriteLine("Welcome to the Ice Scramble Land System!");
 
-        // Display available flavor with prices
+        string[] flavors = { "Ube", "Strawberry" };
+        string[] syrups = { "Chocolate", "Milk" };
+        string[] toppings = { "Sprinkles", "Marshmallow" };
+
         Console.WriteLine("Available Flavor:");
-        Console.WriteLine("1. Ube (₱20)");
-        Console.WriteLine("2. Strawberry (₱25)");
+        for (int i = 0; i < flavors.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {flavors[i]} (₱{20 + i * 5})");
+        }
 
-        string flavor = GetValidFlavorSelection();
+        int flavorIndex = GetValidSelection("flavor", flavors.Length);
+        string flavor = flavors[flavorIndex - 1];
 
-        // Display available syrup with prices
         Console.WriteLine("\nAvailable Syrup:");
-        Console.WriteLine("1. Chocolate (₱10)");
-        Console.WriteLine("2. Milk (₱5)");
+        for (int i = 0; i < syrups.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {syrups[i]} (₱{10 - i * 5})");
+        }
 
-        string syrup = GetValidSyrupSelection();
+        int syrupIndex = GetValidSelection("syrup", syrups.Length);
+        string syrup = syrups[syrupIndex - 1];
 
-        // Display available toppings with prices
         Console.WriteLine("\nAvailable Toppings:");
-        Console.WriteLine("1. Sprinkles (₱5)");
-        Console.WriteLine("2. Marshmallow (₱8)");
+        for (int i = 0; i < toppings.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {toppings[i]} (₱{5 + i * 3})");
+        }
 
-        string toppings = GetValidToppingsSelection();
+        int toppingsIndex = GetValidSelection("toppings", toppings.Length);
+        string selectedToppings = toppings[toppingsIndex - 1];
 
-        // Calculate the total price
-        int total = GetTotalPrice(flavor, syrup, toppings);
+        int total = GetFlavorPrice(flavor) + GetSyrupPrice(syrup) + GetToppingsPrice(selectedToppings);
 
-        // Display the order summary
         Console.WriteLine("\nYour order is:");
         Console.WriteLine($"- {flavor} Flavor");
         Console.WriteLine($"- {syrup} Syrup");
-        Console.WriteLine($"- {toppings} Toppings");
+        Console.WriteLine($"- {selectedToppings} Toppings");
         Console.WriteLine($"Total Price: ₱{total}");
 
-        // Wait for user input before closing the program
         Console.WriteLine("\nThank you for ordering! Have a nice day ^_^");
         Console.WriteLine("\nPress any key to exit...");
         Console.ReadKey();
     }
 
-    static string GetValidFlavorSelection()
+    static int GetValidSelection(string item, int length)
     {
-        string flavorInput;
-        string flavor;
+        int selectedIndex;
         bool validSelection = false;
 
         do
         {
-            Console.WriteLine("Please enter the number for your desired flavor:");
-            flavorInput = Console.ReadLine();
-            switch (flavorInput)
+            Console.WriteLine($"Please enter the number for your desired {item}:");
+            string input = Console.ReadLine();
+            validSelection = int.TryParse(input, out selectedIndex) && selectedIndex >= 1 && selectedIndex <= length;
+
+            if (!validSelection)
             {
-                case "1":
-                    flavor = "Ube";
-                    validSelection = true;
-                    break;
-                case "2":
-                    flavor = "Strawberry";
-                    validSelection = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid flavor selection! Please try again.");
-                    flavor = "";
-                    break;
+                Console.WriteLine($"Invalid {item} selection! Please try again.");
             }
         } while (!validSelection);
 
-        return flavor;
-    }
-
-    static string GetValidSyrupSelection()
-    {
-        string syrupInput;
-        string syrup;
-        bool validSelection = false;
-
-        do
-        {
-            Console.WriteLine("Please enter the number for your desired syrup:");
-            syrupInput = Console.ReadLine();
-            switch (syrupInput)
-            {
-                case "1":
-                    syrup = "Chocolate";
-                    validSelection = true;
-                    break;
-                case "2":
-                    syrup = "Milk";
-                    validSelection = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid syrup selection! Please try again.");
-                    syrup = "";
-                    break;
-            }
-        } while (!validSelection);
-
-        return syrup;
-    }
-
-    static string GetValidToppingsSelection()
-    {
-        string toppingsInput;
-        string toppings;
-        bool validSelection = false;
-
-        do
-        {
-            Console.WriteLine("Please enter the number for your desired toppings:");
-            toppingsInput = Console.ReadLine();
-            switch (toppingsInput)
-            {
-                case "1":
-                    toppings = "Sprinkles";
-                    validSelection = true;
-                    break;
-                case "2":
-                    toppings = "Marshmallow";
-                    validSelection = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid toppings selection! Please try again.");
-                    toppings = "";
-                    break;
-            }
-        } while (!validSelection);
-
-        return toppings;
-    }
-
-    static int GetTotalPrice(string flavor, string syrup, string toppings)
-    {
-        int flavorPrice = GetFlavorPrice(flavor);
-        int syrupPrice = GetSyrupPrice(syrup);
-        int toppingsPrice = GetToppingsPrice(toppings);
-
-        return flavorPrice + syrupPrice + toppingsPrice;
+        return selectedIndex;
     }
 
     static int GetFlavorPrice(string flavor)
