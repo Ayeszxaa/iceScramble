9d8b2d4db715a73fee99369d7a6f6a75d34f4acb
Ayeszxaa <130112710+Ayeszxaa@users.noreply.github.com>
2023-06-20 13:12:50 +0800

    LIST

--- iceScramble/Program.cs
+++ iceScramble/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//JIMENEZ, BLESSIE MAE AYESSA D.
+using System;
+using System.Collections.Generic;
 
 class IceScramble
 {
@@ -6,77 +8,98 @@ class IceScramble
     {
         Console.WriteLine("Welcome to the Ice Scramble Land System!");
 
-        string[] flavors = { "Ube", "Strawberry" };
-        string[] syrups = { "Chocolate", "Milk" };
-        string[] toppings = { "Sprinkles", "Marshmallow" };
+        // Define available flavors with prices
+        List<string> flavors = new List<string> { "Ube (₱20)", "Strawberry (₱25)" };
 
+        // Display available flavors with prices
         Console.WriteLine("Available Flavor:");
-        for (int i = 0; i < flavors.Length; i++)
-        {
-            Console.WriteLine($"{i + 1}. {flavors[i]} (₱{20 + i * 5})");
-        }
+        DisplayMenuItems(flavors);
 
-        int flavorIndex = GetValidSelection("flavor", flavors.Length);
-        string flavor = flavors[flavorIndex - 1];
+        string flavor = GetValidSelection(flavors);
 
+        // Define available syrups with prices
+        List<string> syrups = new List<string> { "Chocolate (₱10)", "Milk (₱5)" };
+
+        // Display available syrups with prices
         Console.WriteLine("\nAvailable Syrup:");
-        for (int i = 0; i < syrups.Length; i++)
-        {
-            Console.WriteLine($"{i + 1}. {syrups[i]} (₱{10 - i * 5})");
-        }
+        DisplayMenuItems(syrups);
 
-        int syrupIndex = GetValidSelection("syrup", syrups.Length);
-        string syrup = syrups[syrupIndex - 1];
+        string syrup = GetValidSelection(syrups);
 
+        // Define available toppings with prices
+        List<string> toppings = new List<string> { "Sprinkles (₱5)", "Marshmallow (₱8)" };
+
+        // Display available toppings with prices
         Console.WriteLine("\nAvailable Toppings:");
-        for (int i = 0; i < toppings.Length; i++)
-        {
-            Console.WriteLine($"{i + 1}. {toppings[i]} (₱{5 + i * 3})");
-        }
+        DisplayMenuItems(toppings);
 
-        int toppingsIndex = GetValidSelection("toppings", toppings.Length);
-        string selectedToppings = toppings[toppingsIndex - 1];
+        string selectedToppings = GetValidSelection(toppings);
 
-        int total = GetFlavorPrice(flavor) + GetSyrupPrice(syrup) + GetToppingsPrice(selectedToppings);
+        // Calculate the total price
+        int total = GetTotalPrice(flavors, syrup, toppings);
 
+        // Display the order summary
         Console.WriteLine("\nYour order is:");
         Console.WriteLine($"- {flavor} Flavor");
         Console.WriteLine($"- {syrup} Syrup");
         Console.WriteLine($"- {selectedToppings} Toppings");
         Console.WriteLine($"Total Price: ₱{total}");
 
+        // Wait for user input before closing the program
         Console.WriteLine("\nThank you for ordering! Have a nice day ^_^");
         Console.WriteLine("\nPress any key to exit...");
         Console.ReadKey();
     }
 
-    static int GetValidSelection(string item, int length)
+    static void DisplayMenuItems(List<string> items)
     {
+        for (int i = 0; i < items.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {items[i]}");
+        }
+    }
+
+    static string GetValidSelection(List<string> items)
+    {
+        string input;
         int selectedIndex;
         bool validSelection = false;
 
         do
         {
-            Console.WriteLine($"Please enter the number for your desired {item}:");
-            string input = Console.ReadLine();
-            validSelection = int.TryParse(input, out selectedIndex) && selectedIndex >= 1 && selectedIndex <= length;
+            Console.WriteLine("Please enter the number for your desired selection:");
+            input = Console.ReadLine();
 
-            if (!validSelection)
+            if (int.TryParse(input, out selectedIndex) && selectedIndex >= 1 && selectedIndex <= items.Count)
             {
-                Console.WriteLine($"Invalid {item} selection! Please try again.");
+                validSelection = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection! Please try again.");
             }
         } while (!validSelection);
 
-        return selectedIndex;
+        return items[selectedIndex - 1];
     }
 
-    static int GetFlavorPrice(string flavor)
+    static int GetTotalPrice(List<string> flavors, string syrup, List<string> toppings)
     {
+        int flavorPrice = GetFlavorPrice(flavors);
+        int syrupPrice = GetSyrupPrice(syrup);
+        int toppingsPrice = GetToppingsPrice(toppings);
+
+        return flavorPrice + syrupPrice + toppingsPrice;
+    }
+
+    static int GetFlavorPrice(List<string> flavors)
+    {
+        string flavor = flavors[0]; // Default to the first flavor
         switch (flavor)
         {
-            case "Ube":
+            case "Ube (₱20)":
                 return 20;
-            case "Strawberry":
+            case "Strawberry (₱25)":
                 return 25;
             default:
                 return 0;
@@ -87,26 +110,32 @@ class IceScramble
     {
         switch (syrup)
         {
-            case "Chocolate":
+            case "Chocolate (₱10)":
                 return 10;
-            case "Milk":
+            case "Milk (₱5)":
                 return 5;
             default:
                 return 0;
         }
     }
 
-    static int GetToppingsPrice(string toppings)
+    static int GetToppingsPrice(List<string> toppings)
     {
-        switch (toppings)
+        int totalPrice = 0;
+        foreach (string topping in toppings)
         {
-            case "Sprinkles":
-                return 5;
-            case "Marshmallow":
-                return 8;
-            default:
-                return 0;
+            switch (topping)
+            {
+                case "Sprinkles (₱5)":
+                    totalPrice += 5;
+                    break;
+                case "Marshmallow (₱8)":
+                    totalPrice += 8;
+                    break;
+                default:
+                    break;
+            }
         }
+        return totalPrice;
     }
 }
-
